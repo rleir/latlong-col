365ef492cd82f67d9eb2619ac606fdf39b0ed499
Rick Leir <rleir@leirtech.com>
2019-07-04 19:53:09 -0400

    pep8 fixes, no functional change

--- src/addLatLong.py
+++ src/addLatLong.py
@@ -21,7 +21,7 @@ __maintainer__ = "Richard Leir"
 __email__ = "rleir at leirtech ddot com"
 __status__ = "Production"
 
-from xlrd import open_workbook # type: ignore
+from xlrd import open_workbook  # type: ignore
 from typing import Dict, List
 import os
 import geopy    # pip install geopy
@@ -36,12 +36,13 @@ inputfilename = "input.xlsx"
 locFileName = 'locations.json'
 
 # Output locations-with-popup file
-locInstFilename ='locationsInstitutions.json'
+locInstFilename = 'locationsInstitutions.json'
 
-all_data   = {} # type: Dict
+all_data = {}  # type: Dict
 
 INST_DEPT_LABEL = "Inst Dept"
 INST_NAME_LABEL = "Inst Name"
+
 
 def readFiles() -> None:
     global all_data
@@ -51,6 +52,7 @@ def readFiles() -> None:
         for addr in all_data:
             all_data[addr]["count"] = 0
 
+
 def scanSpreadsheet():
     # read xls, find avg sheet
     # path = "../data/revnAcq.xlsx"
@@ -58,16 +60,16 @@ def scanSpreadsheet():
     wb = open_workbook(path)
     s_found    = None
     addrCols   = None
-    orgCols   = None
+    orgCols    = None
     for sheet in wb.sheets():
         shname = sheet.name
         if shname.endswith("Acq xlsx"):
             s_found = 1
             for row in range(sheet.nrows):
                 if row == 0:
-                    addrCols = getAddressColumns(sheet,row)
+                    addrCols = getAddressColumns(sheet, row)
                 else:
-                    getRowAddress(sheet,row,addrCols)
+                    getRowAddress(sheet, row, addrCols)
             # get any latlong info
             getInfo()
 
@@ -77,16 +79,17 @@ def scanSpreadsheet():
             # add the Institution names
             for row in range(sheet.nrows):
                 if row == 0:
-                    orgCols  = getOrgColumns(sheet,row)
+                    orgCols  = getOrgColumns(sheet, row)
                 else:
-                    addInstNames(sheet,row,addrCols,orgCols)
+                    addInstNames(sheet, row, addrCols, orgCols)
             # write augmented data to a different file
             writeFile(locInstFilename)
 
-    if s_found is None :
+    if s_found is None:
         print("sheet not found in " + path)
 
-def getAddressColumns(sheet,row):
+
+def getAddressColumns(sheet, row):
     ''' determine which spreadsheet columns contain address info '''
     cityCol    = None
     provCol    = None
@@ -94,12 +97,12 @@ def getAddressColumns(sheet,row):
     addrCols   = None
 
     for col in range(sheet.ncols):
-        hdr = sheet.cell(row,col).value
-        if "City" == hdr :
+        hdr = sheet.cell(row, col).value
+        if "City" == hdr:
             cityCol = col
-        elif "Prov./state" == hdr :
+        elif "Prov./state" == hdr:
             provCol = col
-        elif "Country" == hdr :
+        elif "Country" == hdr:
             countryCol = col
 
         if cityCol is None:
@@ -111,20 +114,21 @@ def getAddressColumns(sheet,row):
         if cityCol is None and provCol is None and countryCol is None:
             print(" no addr cols found")
         else:
-            addrCols = (  cityCol, provCol, countryCol)
+            addrCols = (cityCol,  provCol,  countryCol)
     return addrCols
 
-def getOrgColumns(sheet,row):
+
+def getOrgColumns(sheet, row):
     ''' determine which spreadsheet columns contain organization name info '''
     deptCol    = None
     nameCol    = None
     orgCols    = None
 
     for col in range(sheet.ncols):
-        hdr = sheet.cell(row,col).value
-        if INST_DEPT_LABEL == hdr :
+        hdr = sheet.cell(row, col).value
+        if INST_DEPT_LABEL == hdr:
             deptCol = col
-        elif INST_NAME_LABEL == hdr :
+        elif INST_NAME_LABEL == hdr:
             nameCol = col
 
         if deptCol is None:
@@ -134,20 +138,21 @@ def getOrgColumns(sheet,row):
         if deptCol is None and nameCol is None:
             print(" no org cols found")
         else:
-            orgCols = (  deptCol, nameCol)
+            orgCols = (deptCol, nameCol)
     return orgCols
 
-def getRowAddress(sheet,row,addrCols):
+
+def getRowAddress(sheet, row, addrCols):
     ''' get address info from a spreadsheet row '''
     global all_data
     addr = ""
     for col in range(sheet.ncols):
         if col in addrCols:
-            addr += sheet.cell(row,col).value
+            addr += sheet.cell(row, col).value
             addr += ' '
-    addr = addr.rstrip() # remove the last space
+    addr = addr.rstrip()  # remove the last space
 
-    if not( addr == ""):
+    if not(addr == ""):
         if addr in all_data.keys():
             all_data[addr]["count"] += 1
 
@@ -155,48 +160,52 @@ def getRowAddress(sheet,row,addrCols):
             geo_loc = {}
             geo_loc["count"] = 1
             geo_loc["org names"] = []
-            all_data[ addr] = geo_loc
+            all_data[addr] = geo_loc
 
-def addInstNames(sheet,row,addrCols,orgCols):
+
+def addInstNames(sheet,  row,  addrCols,  orgCols):
     ''' get address info from a spreadsheet row '''
     global all_data
     addr = ""
     orgName = ""
     for col in range(sheet.ncols):
-        # recreate addr same as we did above (could use a list and remember it instead)
+        # recreate addr same as we did above
+        #    (could use a list and remember it instead)
         if col in addrCols:
-            addr += sheet.cell(row,col).value
+            addr += sheet.cell(row, col).value
             addr += ' '
 
         if col in orgCols:
             if col is orgCols[0]:
-                orgDept = sheet.cell(row,col).value
-                if not orgDept is "":
+                orgDept = sheet.cell(row, col).value
+                if orgDept is not "":
                     orgName += orgDept
                     orgName += ', '
             elif col is orgCols[1]:
-                orgInst = sheet.cell(row,col).value
-                if not orgInst is "":
+                orgInst = sheet.cell(row, col).value
+                if orgInst is not "":
                     orgName += orgInst
-    # Do not show anything starting with 'Estate ', because it will be followed by a person's name
+    # Do not show anything starting with 'Estate ',
+    #   for privacy: it will be followed by a person's name
     if orgName.startswith("Estate "):
         orgName = ""
 
-    addr    =    addr.rstrip() # remove the last space
-    if not( addr == ""):
+    addr = addr.rstrip()  # remove the last space
+    if not(addr == ""):
         if addr in all_data.keys():
 
-            if not orgName is "":
-                if not "org names" in all_data[addr].keys():
-                    all_data[addr]["org names"] = {orgName:1}
+            if orgName is not "":
+                if "org names" not in all_data[addr].keys():
+                    all_data[addr]["org names"] = {orgName: 1}
                 else:
                     if orgName in all_data[addr]["org names"]:
                         all_data[addr]["org names"][orgName] += 1
                     else:
-                        all_data[addr]["org names"][orgName]=1
+                        all_data[addr]["org names"][orgName] = 1
 
         else:
             print("===addr not found " + addr)
+
 
 def getInfo() -> None:
     ''' Google lat lon position for each address '''
@@ -208,27 +217,29 @@ def getInfo() -> None:
     count = 0
     for addr in all_data:
         # if we already have location data, then skip to the next addr
-        geo_loc = all_data[ addr]
+        geo_loc = all_data[addr]
         if "lat" in geo_loc:
             continue
 
         location = None
-        #addr = "Québec  quebec canada"
+
         try:
             location = g.geocode(addr)
-            geo_loc[ "lat" ]    = location.latitude
-            geo_loc[ "lon" ]    = location.longitude
-            #geo_loc[ "id"  ]    = location.place_id
-            geo_loc[ "address" ]= location.address
+            geo_loc["lat"]     = location.latitude
+            geo_loc["lon"]     = location.longitude
+            # geo_loc["id"]    = location.place_id
+            geo_loc["address"] = location.address
 
-            all_data[ addr] = geo_loc
-            # print( location) # gives  location.address
-            # print( location.raw)
+            all_data[addr] = geo_loc
+            # print(location) # gives  location.address
+            # print(location.raw)
 
-            #zzz print(addr[:50] + ' -- ' + location.address)
-            #print('Lat/Lon: {0}, {1}'.format(location.latitude,location.longitude))
-            #zzz print('https://www.google.ca/maps/@{0},{1},17z'.format(location.latitude,location.longitude))
-        except (Exception, geopy.exc.GeocoderQueryError) as err:
+            # print(addr[:50] + ' -- ' + location.address)
+            # print('Lat/Lon: {0}, {1}'
+            #       .format(location.latitude,location.longitude))
+            # print('https://www.google.ca/maps/@{0},{1},17z'
+            #       .format(location.latitude,location.longitude))
+        except (Exception,  geopy.exc.GeocoderQueryError) as err:
             print("geopy error: {0}".format(err))
             print('... Failed to get a location for {0}'.format(addr))
 
@@ -236,13 +247,15 @@ def getInfo() -> None:
             return
         count = count+1
 
+
 def writeFile(filename) -> None:
     global all_data
     with open(filename, 'w', encoding='utf8') as json_file:
         json.dump(all_data, json_file)
 
+
 if __name__ == "__main__":
-# execute only if run as a script
+    # execute only if run as a script
 
     readFiles()
     scanSpreadsheet()
