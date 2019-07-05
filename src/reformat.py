4a9f83829541d7e55c9a061bd4e3b99237c3ee64
Rick Leir <rleir@leirtech.com>
2019-07-05 11:31:04 -0400

    pep8 changes only, no functional changes

--- src/reformat.py
+++ src/reformat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Handy utility to reformat the locations file for use in a d3js map.  
+Handy utility to reformat the locations file for use in a d3js map.
   read input locations data file
   for each location, generate a feature record
   write the features data file
@@ -15,16 +15,16 @@ __maintainer__ = "Richard Leir"
 __email__ = "rleir at leirtech ddot com"
 __status__ = "Production"
 
-from xlrd import open_workbook # type: ignore
+from xlrd import open_workbook  # type: ignore
 from typing import Dict, List
 import os
-import geopy    # pip install geopy
+import geopy
 import geopy.geocoders
 from geopy.geocoders import GoogleV3
 import json
 
-all_data   = {} # type: Dict
-fea_data   = {} # type: Dict
+all_data = {}  # type: Dict
+fea_data = {}  # type: Dict
 
 # Input locations-with-popup file
 locFileName = 'locationsInstitutions.json'
@@ -32,69 +32,69 @@ locFileName = 'locationsInstitutions.json'
 # Output GeoJson file
 feaFileName = 'acquisitions.json'
 
+
 def readFiles() -> None:
     global all_data
     # read existing locations, zero each count
     with open(locFileName) as json_file:
         all_data = json.load(json_file)
 
+
 def reformatInfo() -> None:
     ''' generate feature rec with lat lon position for each address '''
 
     global all_data
     global fea_data
-    fea_data[ "type"] = "FeatureCollection"
+    fea_data["type"] = "FeatureCollection"
     metadata = {}  # some dummy metadata
-    metadata[ "generated"]= 1559586926000 # dummy
-    metadata[ "url"]= "https://zzzz/" # dummy
-    metadata[ "title"]= "zzz" # dummy
-    metadata[ "status"]= 200 # dummy
-    metadata[ "api"]= "1.8.1" # dummy
-    metadata[ "count"]= len(all_data)
-    fea_data[ "metadata"] = metadata
+    metadata["generated"] = 1559586926000   # dummy
+    metadata["url"] = "https://zzzz/"       # dummy
+    metadata["title"] = "zzz"               # dummy
+    metadata["status"] = 200                # dummy
+    metadata["api"] = "1.8.1"               # dummy
+    metadata["count"] = len(all_data)
+    fea_data["metadata"] = metadata
 
     features = []
-    fea_data[ "features"] = features
+    fea_data["features"] = features
 
     for addr in all_data:
         feature = {}
-        properties = {}        
+        properties = {}
         geometry = {}
 
-        if not "address" in all_data[addr] : # check for key existence
-            continue # skip this record
-        
-        properties["place"]=  all_data[addr]["address"]
-        properties["mag"]=    float( all_data[addr]["count"])/10
+        if "address" not in all_data[addr]:  # check for key existence
+            continue    # skip this record
+
+        properties["place"] = all_data[addr]["address"]
+        properties["mag"] = float(all_data[addr]["count"])/10
 
         if "org names" in all_data[addr]:
-            properties["popupContent"] = all_data[addr][ "org names"]
+            properties["popupContent"] = all_data[addr]["org names"]
 
         coordinates = []
-        coordinates.append(   all_data[addr]["lon"])
-        coordinates.append(   all_data[addr]["lat"])
+        coordinates.append(all_data[addr]["lon"])
+        coordinates.append(all_data[addr]["lat"])
         coordinates.append(9)
         geometry["type"] = "Point"
-        geometry["coordinates"]= coordinates
+        geometry["coordinates"] = coordinates
 
-        feature[ "type"] = "Feature"
-        feature[ "properties"] = properties
-        feature[ "geometry"] = geometry
-        feature[ "id"] = "zzz"
-        features.append( feature)
-        
+        feature["type"] = "Feature"
+        feature["properties"] = properties
+        feature["geometry"] = geometry
+        feature["id"] = "zzz"
+        features.append(feature)
+
+
 def writeFiles() -> None:
     global all_data
     with open(feaFileName, 'w', encoding='utf8') as json_file:
         json.dump(fea_data, json_file)
 
+
 if __name__ == "__main__":
-# execute only if run as a script
+    # execute only if run as a script
 
     readFiles()
     reformatInfo()
     writeFiles()
-
-    
-
-
