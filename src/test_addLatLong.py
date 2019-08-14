41c491da4715677fc032fcd952284f2844641125
Rick Leir <rleir@leirtech.com>
2019-08-14 13:52:29 -0400

    add a test fix filenames

--- src/test_addLatLong.py
+++ src/test_addLatLong.py
@@ -11,7 +11,7 @@ test_locFileName = "testData/testLoc.json"
 
 # output file names for several tests
 test_locCountsFileName = "testData/testLocCounts.json"
-test_locInstFileName   = "testData/testlocInst.json"
+test_locInstFileName   = "testData/testLocInst.json"
 
 
 def init_test_loc_file():
@@ -68,7 +68,7 @@ def test_b_one_row():
     # output locations DB should be unchanged,
     #     and should equal the test check file
     assert filecmp.cmp(test_locFileName,
-                       "testData/test_B_oneLocRef.json", shallow=False)
+                       test_initlocFileName, shallow=False)
 
     # output locations counts and inst files
     #      should equal the test reference files
@@ -76,7 +76,7 @@ def test_b_one_row():
                        "testData/test_B_oneLocCountsRef.json", shallow=False)
 
     assert filecmp.cmp(test_locInstFileName,
-                       "testData/test_B_oneInstRef.json", shallow=False)
+                       "testData/test_B_oneLocInstRef.json", shallow=False)
     return True
 
 
@@ -157,5 +157,30 @@ def test_c_two_rows_same_addr():
     # output loc Inst files should equal the test check files
     assert filecmp.cmp(test_locInstFileName,
                        "testData/test_C_twoLocInstRef.json", shallow=False)
+    return True
 
+
+def test_a_one_row():
+    init_test_loc_file()
+    a1 = addLatLong.AcqInfo(test_locFileName,
+                            test_locCountsFileName,
+                            test_locInstFileName)
+
+    # test with one row in the input, having InstName, Prov, Country
+    a1.scan_spreadsheet("testData/test_A_one.xlsx")
+    assert a1.all_data["Russia"]["count"] == 1
+    a1.write_location_DB()
+
+    # output locations DB should be unchanged,
+    #     and should equal the test check file
+    assert filecmp.cmp(test_locFileName,
+                       "testData/test_B_oneLocRef.json", shallow=False)
+
+    # output locations counts and inst files
+    #      should equal the test reference files
+    assert filecmp.cmp(test_locCountsFileName,
+                       "testData/test_A_oneLocCountsRef.json", shallow=False)
+
+    assert filecmp.cmp(test_locInstFileName,
+                       "testData/test_A_oneInstRef.json", shallow=False)
     return True
